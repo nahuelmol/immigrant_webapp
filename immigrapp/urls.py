3481c4cdcbe40f9e599afb52dd7bfa113271456b
_moliNasil <41929823+nahuelmol@users.noreply.github.com>
2021-03-05 18:25:29 -0300

    Update urls.py

--- immigrapp/urls.py
+++ immigrapp/urls.py
@@ -9,6 +9,6 @@ from . import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('account_user/',include('account.urls'),namespace='users'),
-    path('account_monitor/',include(account.urls),namespace='monitor'),
+    path('account_monitor/',include('account.urls'),namespace='monitor'),
     path('home/',include('immiprocess.urls'))
 ]
