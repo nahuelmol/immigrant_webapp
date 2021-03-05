2c608142d765da57472a19a3ce811e0cc3b46d75
_moliNasil <41929823+nahuelmol@users.noreply.github.com>
2021-03-05 18:15:29 -0300

    Update urls.py

--- account/urls.py
+++ account/urls.py
@@ -3,7 +3,10 @@ from django.urls import path, include
 
 from . import views
 
+app_name = 'account'
+
 urlpatterns = [
+        path('menu/', views.homepage,name='homepage'),
 	path('register/', views.register_page, name='register'),
 	path('login/', views.login_page, name='login')
 ]
