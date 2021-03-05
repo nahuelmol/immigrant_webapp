32d5d6708b7264f03482acf20a3788b82119f1b1
_moliNasil <41929823+nahuelmol@users.noreply.github.com>
2021-03-05 18:39:39 -0300

    Update views.py

--- immiprocess/views.py
+++ immiprocess/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
+from account.views import login_page
+from django.decorators import login_requiered
 
 def homepage(req):
 	context = {}
 	return render(req,'main/homepage.html',context)
 
+#@login_requiered(login_url='login')
 def service_params(req):
 	context = {}
 	return render(req,'process/manuparams.html',context)
